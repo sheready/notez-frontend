af41f403786a051e6639c12869431baf514b1b87
sheready <csstlw@gmail.com>
2022-12-20 09:57:40 +0300

    map notes date from the api response

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const NotesListPage = () => {
+
+    let [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    let getNotes = async () => {
+        let response = await fetch('http://127.0.0.1:8000/api/notes/')
+        let data = await response.json()
+        console.log('DATA:',data)
+        setNotes(data)
+    }
   return (
     <div>
-        notes
+        <div className='notes-list'>
+            {
+                notes.map((note, index) => (
+                    <h3> {note.body} </h3>
+                ))
+            }
+
+        </div>
     </div>
   )
 }
