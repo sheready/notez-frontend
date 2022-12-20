464951559953eaa9eac9b0d203ede6c6174963e3
sheready <csstlw@gmail.com>
2022-12-20 09:58:24 +0300

    add key to notes data

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -19,7 +19,7 @@ const NotesListPage = () => {
         <div className='notes-list'>
             {
                 notes.map((note, index) => (
-                    <h3> {note.body} </h3>
+                    <h3 key={index}> {note.body} </h3>
                 ))
             }
 
