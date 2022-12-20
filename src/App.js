e7fe646dc7fedb2f33304493dfba070c322f09ca
sheready <csstlw@gmail.com>
2022-12-20 11:31:08 +0300

    use react router 6 to render components header and notelist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage';
 
 
 function App() {
   return (
-    <div className="App">
-     <Header />
-     <NotesListPage />
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={
+            <>
+            <Header/>
+            <NotesListPage/>
+            </>
+          }/>
+      </Routes>
+
+    </Router>
+
   );
 }
 
