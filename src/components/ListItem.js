4451ceaebc2ac00c4d11700854c941f293e818cb
sheready <csstlw@gmail.com>
2022-12-22 14:52:48 +0300

    add material ui card component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,14 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
 
 // pass notes as a prop
 const ListItem = ({ note }) => {
-  return (
-    <div>
+  
+const card = (
+
+  <React.Fragment>
+    <CardContent>
+      <Typography variant="h5" component="div">
+        {/* be{bull}nev{bull}o{bull}lent */}
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+      </Typography>
+      <Typography variant="body2">
+        {note.body}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">
       <Link to={`/note/${note.id}`}>
         {/* display notes body */}
-        <h3>{note.body}</h3>
+        <h3>See more</h3>
+        
       </Link>
+      </Button>
+    </CardActions>
+  </React.Fragment>
+  );
+
+  return (
+    <div>
+        <Box sx={{ minWidth: 275 }}>
+          <Card variant="outlined">{card}</Card>
+        </Box>
+       
         
     </div>
   )
