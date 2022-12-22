b2a6788f11cdc86716798ed36388d5a351b4fd15
sheready <csstlw@gmail.com>
2022-12-22 10:10:37 +0300

    add Link from react dom to allow us to access the notepage on click

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // pass notes as a prop
 const ListItem = ({ note }) => {
   return (
     <div>
+      <Link to={`/note/${note.id}`}>
         {/* display notes body */}
         <h3>{note.body}</h3>
+      </Link>
+        
     </div>
   )
 }
