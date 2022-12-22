292ef36e88068b595ac0f94ee7abaf0f2ae2668a
sheready <csstlw@gmail.com>
2022-12-22 21:25:18 +0300

    add the navigate to home on handlesubmit

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -8,6 +8,7 @@ import Typography from '@mui/material/Typography';
 import { Button } from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { Link } from "react-router-dom";
+import TextareaAutosize from '@mui/base/TextareaAutosize';
 
 
 
@@ -17,6 +18,7 @@ const NotePage = () => {
     // The useParams hook returns an object of key/value pairs of the dynamic params from 
     //the current URL that were matched by the <Route path>.
     let noteId = useParams().id
+    let navigate = useNavigate();
 
     useEffect(() => {
         getNote()
@@ -29,42 +31,56 @@ const NotePage = () => {
         setNote(data)
     }
 
+    let updateNote = async() => {
+      fetch(`/api/notes/${noteId}/update/`,{
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+      })
+    }
+
+    let handleSubmit = () =>{
+      updateNote()
+      navigate('/')
+    }
+
     const card = (
 
         <React.Fragment>
           <CardContent>
             <Typography variant="h5" component="div">
-              {/* be{bull}nev{bull}o{bull}lent */}
             </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            <Typography sx={{ mb: 1.5 }} color="text.secondary"> 
             </Typography>
             <Typography variant="body2">
-                {/* checks if we have a note body and passes it  */}
                 {note?.body}
             </Typography>
+            <TextareaAutosize
+              aria-label="empty textarea"
+              placeholder={note?.body}
+              style={{ width: 200 }}
+              onChange = {(e) => {setNote({...note, 'body':e.target.value})}}
+            />
           </CardContent>
           <CardActions>
-            <Button size="small">
-                <Link to={`/`}>
-                    <ArrowBackIosNewIcon />
-                </Link>
-                </Button>
+            <Button size="small"> 
+              <ArrowBackIosNewIcon  onClick={handleSubmit}/>
+            </Button>
                 
             
           </CardActions>
         </React.Fragment>
         );
 
- 
-    
-
-   
-
   return (
     <div>
-        <Box sx={{ minWidth: 275 }}>
+        <Box sx={{ minWidth: 275 }} id="itemcard" className='notes-list'>
           <Card variant="outlined">{card}</Card>
+
         </Box>
+  
       
 
     </div>
