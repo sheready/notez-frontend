942d5679fd5b539b897c791277b21264ef4682ee
sheready <csstlw@gmail.com>
2022-12-22 14:52:03 +0300

    add header to display in all components

--- src/App.js
+++ src/App.js
@@ -9,18 +9,27 @@ import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage';
 import NotePage from './pages/NotePage';
 
+
+
+
 function App() {
+
+
   return (
     <Router>
+      <Header/>
       <Routes>
-          <Route path="/" exact element={
-            <>
-            <Header/>
-            <NotesListPage/>
-            </>
-          }/>
-          <Route path="/note/:id" element={<NotePage/>} />
+        
+        <Route path="/" exact element={
+          <>
+         
+          <NotesListPage/>
+          </>
+        }/>
+        <Route path="/note/:id" element={<NotePage/>} />
       </Routes>
+
+
 
     </Router>
 
